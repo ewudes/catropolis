a9b391253c150609e30252e11a6a63e7cca6d1f1
ewudes <ewudes@gmail.com>
2026-08-21 14:02:04 +0300

    fix: routers

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,8 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: '', renderMode: RenderMode.Prerender },
+  { path: 'catalog', renderMode: RenderMode.Prerender },
+  { path: 'form', renderMode: RenderMode.Prerender },
+  { path: '**', renderMode: RenderMode.Server }
 ];
